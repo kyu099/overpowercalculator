358ed89a789d708ff097747c9f52724014064767
kyu099 <ryu_gelus1277@icloud.com>
2023-08-09 00:22:33 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+const prevscore = document.getElementById("prevscore");
+const afterscore = document.getElementById("afterscore");
+const out1 = document.getElementById("out1");
+
+out1.innerHTML = prevscore * 0.0015;
